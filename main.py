1f0e557e2072bc33a5c8f5c57a5bafb92c6d4409
Olivier Remy <olivier.remy.paris@gmail.com>
2022-11-09 21:25:27 +0100

    Ajout de fonctions

--- main.py
+++ main.py
@@ -6,14 +6,133 @@ url_livre = "http://books.toscrape.com/catalogue/love-is-a-mix-tape-music-1_711/
 
 
 # Utilisation de requests/beautifulsoup4 pour parser le Html
-def html_parser():
+def html_parser_site():
     response = requests.get(url_site)
-    soup_html = bs(response.content, 'html.parser')
-    return soup_html
+    soup_site = bs(response.content, 'html.parser')
+    return soup_site
 
+
+def html_parser_livre():
+    response = requests.get(url_livre)
+    soup_livre = bs(response.content, 'html.parser')
+    return soup_livre
+
+
+def get_livres_url():
+    main_html = html_parser_site()
+
+
+def get_produit_url():
+    content = url_livre
+    return content
+
+
+# On récupère un dictionnaire des infos code prix et stock
+def get_produit_code_prix_stock():
+    main_html = html_parser_livre()
+    content = main_html.find('table', class_='table table-striped')
+    code_upc = content.findAll('tr')
+    infos = {}
+    for ifs in code_upc:
+        infos[ifs.findChildren()[0].text] = ifs.findChildren()[1].text
+    return infos
+
+
+# On créer un objet infos avec la methode pour selectionner la clé
+def get_code_produit():
+    infos = get_produit_code_prix_stock()
+    return infos['UPC']
+
+
+def get_prix_ht():
+    infos = get_produit_code_prix_stock()
+    return infos['Price (excl. tax)']
+
+
+def get_prix_ttc():
+    infos = get_produit_code_prix_stock()
+    return infos['Price (incl. tax)']
+
+
+def get_stock():
+    infos = get_produit_code_prix_stock()
+    return infos['Availability']
+
+
+def get_page_url():
+    main_html = html_parser_site()
+    content = main_html.find('a')
+    link = '{}{}'.format(url_site, content['href'])
+    return link
+
+
+"""
+    books_link = get_all_books_url(data)
+         for book_url in  books_link:
+            print('data collection : ', book_url)
+            try:
+               data = get_book_info(book_url)
+"""
+
+
+def get_titre():
+    main_html = html_parser_livre()
+    titre = main_html.find('title').text
+    titre = titre.strip()
+    return titre
+
+
+def get_categorie():
+    main_html = html_parser_livre()
+    categorie = main_html.find('ul', class_='breadcrumb')
+    get_links = categorie.findAll('a')
+    catego = []
+    categos = []
+    for cat in get_links:
+        catego = cat.text
+        categos.append(catego)
+    # Bloqué 1h à cause de l'indentation et list()[]
+    catego = list(categos)[2]
+    return catego
+
+
+# content_inner > article > div.row > div.col-sm-6.product_main > p.star-rating.One
+def get_nb_etoiles(): # A finir !!!
+    main_html = html_parser_livre()
+    table_p = main_html.find('div', class_='col-sm-6 product_main')
+    get_all_p = table_p.findAll('p')
+    #print(get_all_p)
+    texte = []
+    texte_p = []
+    for tex in get_all_p:
+        texte = tex
+        texte_p.append(tex)
+    texte_p = list(texte_p)[2]
+    ets = []
+    etss = []
+    for et in texte_p:
+        #ets = et.text
+        #etss.append(et)
+        if 'star-rating One' in et:
+            print("1 étoile sur 5")
+        elif 'star-rating Two' in et:
+            print("2 étoiles sur 5")
+        elif 'star-rating Three' in et:
+            print("3 étoiles sur 5")
+        elif 'star-rating Four' in et:
+            print("4 étoiles sur 5")
+        elif 'star-rating Five' in et:
+            print("2 étoiles sur 5")
+    else:
+        print("Il n'y a pas de note")
+    return texte_p
+        
+    # Bloqué 1h à cause de l'indentation et list()[]
+    #nb_etoiles = list(categos)[2]
+    #return nb_etoiles
 
 def get_categories():
-    main_html = html_parser()
+    main_html = html_parser_site()
     content = main_html.find('ul', class_='nav-list')
     get_links = content.findAll('a')
     liens = []
@@ -26,15 +145,20 @@ def get_categories():
          categories_names.append(categorie_name)
     return liens, categories_names
 
+
 def get_all_books_links():
     categories_url, categories_names = get_categories()
     for categories_url in categories_url:
         links = get_links_categorie()
 
+
 def get_links_categorie(categorie_url):
     main_html = html_parser()
     content = main_html.find('ul', class_='nav-list')
-    get_links = content.findAll('a')
+    get_links = content.findAll('a')# All ou _all???? voir
+    # stack car depuis 2016 'findAll' has been renamed to 'find_all'. Link to official documentation
+
+
     liens = []
     for link in get_links:
       if 'http' not in link:
@@ -43,9 +167,24 @@ def get_links_categorie(categorie_url):
     return liens
 
 
+def get_page(): # A finir... methode test pour parse star-rating One ...
+    # Récupère l'url de la page web
+    main_html = html_parser_livre()
+    # page_url = main_html.find_all("p", class_="star-rating One")
+    # table = main_html.findAll('div', attrs={"class": "star-rating One"})
+    page_url = main_html.p['class']
+    tablep = []
+    tablesp = []
+    for p in page_url:
+        tablep = p
+    tablesp.append(tablep)
+    print(tablesp)
+    return tablep
+
+
 """
     infos_livre = {
-    'product_page_url' : 'page_url'
+    'product_page_url' : 'get_product_url()'
     'universal_product_code(upc)' :
     'title':
     'price_including_tax' :
@@ -80,8 +219,8 @@ def get_categorie():
 # On affiche tous les categories de livres
 """
 if __name__ == "__main__":
-    # html_parser()
-    print(get_categories())
+    # html_parser_site()
+    print(get_page())
 
 #    print(categories)
 #    print("Dans le site Books_to_Scrape, il y a", len(categories), "Categories")
